3dc98fbd4b6df2a6842a05e37d7c1f1a1ede89b7
Bastien Brunnenstein <bastien@brunnenstein.fr>
2013-03-28 16:45:16 +0000

    Script Premake4 et Main

---
+++ premake4.lua
@@ -0,0 +1,42 @@
+
+-- Link automatique avec SFML2
+function sfml_links(tab, dbg, static)
+    local suffix = (static and "-s" or "") .. (dbg and "-d" or "")
+    for _, v in pairs(tab) do
+        links("sfml-" .. v .. suffix)
+    end
+end
+
+-- Projet El-Bombino
+solution "El-Bombino"
+  configurations { "Debug", "Release" }
+  targetdir "bin"
+  objdir "bin/obj"
+  includedirs { "include", "../../SFML2/include" }
+  libdirs "../../SFML2/bin/lib"
+
+  -- Configurations Debug et Release
+  configuration "Debug"
+    defines { "DEBUG" }
+    flags { "Symbols", "ExtraWarnings" }
+    targetsuffix "-d"
+
+  configuration "Release"
+    defines { "NDEBUG" }
+    flags { "OptimizeSpeed", "ExtraWarnings" }
+
+  configuration {}
+
+  -- Le programme du jeu
+  project "elbombino"
+    language "C++"
+    files { "src/**.cpp" }
+    sfml_libs = { "system", "window", "graphics", "audio" }
+
+    configuration "Debug"
+      kind "ConsoleApp"
+      sfml_links(sfml_libs, true, true)
+
+    configuration "Release"
+      kind "WindowedApp"
+      sfml_links(sfml_libs, false, true)
---
+++ src/Main.cpp
@@ -0,0 +1,8 @@
+
+/**
+ * @brief Point d'entrée du programme
+ */
+int main(int argc, char ** argv)
+{
+    return 0;
+}
